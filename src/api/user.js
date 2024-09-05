1ee2b8bfc8000a8fe00111a7809d8d69a8d18d22
beristain171089 <vato_182@hotmail.com>
2024-09-04 20:29:37 -0500

    header user profile

--- src/api/user.js
+++ src/api/user.js
@@ -92,5 +92,30 @@ export class User {
 
     };
 
+    async getUser(accessToken, userId) {
+
+        try {
+
+            const url = `${ENV.API_URL}/${ENV.ENDPOINTS.USER}/${userId}`;
+
+            const params = {
+                headers: {
+                    Authorization: `Bearer ${accessToken}`
+                }
+            };
+
+            const response = await fetch(url, params);
+
+            const result = await response.json();
+
+            if (response.status !== 200) throw result;
+
+            return result;
+
+        } catch (error) {
+            throw error;
+        };
+
+    };
 
 };
